a1133a988d883c7a849ae2602d5f464f3f0e8c46
rich <morenoricardo237@outlook.es>
2016-12-07 11:12:47 -0400

    New feature, Count the commits

--- functions.sh
+++ functions.sh
@@ -30,13 +30,24 @@ find_git_changes () {
   fi
 }
 
+# Based on http://stackoverflow.com/questions/677436/how-to-get-the-git-commit-count
+count_git_commit () {
+  local count=$(git rev-list --count HEAD 2> /dev/null)
+
+  if [[ $count ]]; then
+    git_commits="$count"
+  else
+    git_commits="None"
+  fi
+}
+
 make_git_prompt () {
   if [ -d .git ]; then
-    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtrst\]:\[$dirs_color\]\w\[$txtwht\](\[$branch_color\]$git_branch\[$txtwht\]|\[$commit_color\]$git_commit\[$txtwht\])\[$changes_color\]$git_changes\[$txtrst\]\$ '
+    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtrst\]:\[$dirs_color\]\w\[$txtwht\](\[$branch_color\]$git_branch\[$txtwht\]|\[$commit_color\]$git_commit\[$txtwht\]\[$txtwht\]|\[$commit_color\]$git_commits\[$txtwht\])\[$changes_color\]$git_changes\[$txtrst\]\$ '
   else
     export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtwht\]:\[$dirs_color\]\w\[$txtwht\]\$ '
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_commit; make_git_prompt; find_git_changes; $PROMPT_COMMAND"
+PROMPT_COMMAND="find_git_branch; find_git_commit; make_git_prompt; find_git_changes; count_git_commit; $PROMPT_COMMAND"
 
