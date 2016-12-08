ab21ef8fee624dc3ad2ec007f5054eff5d7d6d7c
rich <morenoricardo237@outlook.es>
2016-12-08 17:43:12 -0400

    New character in the prompt .

--- functions.sh
+++ functions.sh
@@ -43,9 +43,9 @@ count_git_commit () {
 
 make_git_prompt () {
   if [ -d .git ]; then
-    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtrst\]:\[$dirs_color\]\w\[$txtwht\](\[$branch_color\]$git_branch\[$txtwht\]|\[$commit_color\]$git_commit\[$txtwht\]\[$txtwht\]|\[$commit_color\]$git_commits\[$txtwht\])\[$changes_color\]$git_changes\[$txtrst\]\$ '
+    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtrst\]:\[$dirs_color\]\w\[$txtwht\](\[$branch_color\]$git_branch\[$txtwht\]|\[$commit_color\]$git_commit\[$txtwht\]\[$txtwht\]|\[$commit_color\]$git_commits\[$txtwht\])\[$changes_color\]$git_changes\[$txtrst\]\$> '
   else
-    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtwht\]:\[$dirs_color\]\w\[$txtwht\]\$ '
+    export PS1='\[$user_color\]${debian_chroot:+($debian_chroot)}\u\[$txtwht\]:\[$dirs_color\]\w\[$txtwht\]\$> '
   fi
 }
 
